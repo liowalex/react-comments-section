86bdda14e8e4405c11207384f64665aa674df8bf
the0w1 <the0w1@proton.me>
2023-07-27 12:18:50 -0700

    Adds data but doesn't check the formik validation schema, need to remove the onclick on button to get it to work, but then it doesn't call the handleSubmit function

--- src/components/InputField/RegularInput.tsx
+++ src/components/InputField/RegularInput.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './InputField.scss'
 import { useContext } from 'react'
 import { GlobalContext } from '../../context/Provider'
-import EmojiInput from './EmojiInput'
+import { object, string } from "zod";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { toFormikValidationSchema } from 'zod-formik-adapter';
 
 interface RegularInputProps {
-  formStyle?: object
   comId?: string
   mode?: string
   customImg?: string
@@ -15,12 +16,21 @@ interface RegularInputProps {
   imgStyle?: object
   imgDiv?: object
   handleSubmit: Function
-  text: string
-  setText: Function
 }
 
+const contactFormSchema = object({
+  // defines a required field called name
+  name: string({
+    required_error: "Please enter your name",
+  }),
+  // defines a required field called email.
+  // we use the built-in email validator from zod
+  email: string().email("Please enter a valid email"),
+  // defines a required field called message with length constraints of 150-1000 characters.
+  message: string().min(5).max(350),
+});
+
 const RegularInput = ({
-  formStyle,
   imgDiv,
   imgStyle,
   customImg,
@@ -30,80 +40,108 @@ const RegularInput = ({
   comId,
   submitBtnStyle,
   handleSubmit,
-  text,
-  setText
 }: RegularInputProps) => {
   const globalStore: any = useContext(GlobalContext)
 
   return (
-    <form
-      className='form'
-      style={globalStore.formStyle || formStyle}
-      onSubmit={() => handleSubmit}
-    >
-      <div className='userImg' style={imgDiv}>
-        <a
-          target='_blank'
-          href={globalStore.currentUserData.currentUserProfile}
-        >
-          <img
-            src={
-              globalStore.customImg ||
-              customImg ||
-              globalStore.currentUserData.currentUserImg
-            }
-            style={globalStore.imgStyle || imgStyle}
-            alt='userIcon'
-            className='imgdefault'
-          />
-        </a>
-      </div>
-      {globalStore.removeEmoji ? (
-        <input
-          className='postComment'
-          style={
-            mode === 'replyMode' || mode === 'editMode'
-              ? globalStore.replyInputStyle
-              : globalStore.inputStyle || inputStyle
-          }
-          type='text'
-          placeholder='Type your reply here.'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      ) : (
-        <EmojiInput
-          text={text}
-          setText={setText}
-          mode={mode}
-          inputStyle={inputStyle}
-        />
-      )}
-
-      {mode && (
-        <button
-          className='cancelBtn'
-          style={globalStore.cancelBtnStyle || cancelBtnStyle}
-          type='button'
-          onClick={() =>
-            mode === 'editMode'
-              ? globalStore.handleAction(comId, true)
-              : globalStore.handleAction(comId, false)
-          }
-        >
-          Cancel
-        </button>
-      )}
-      <button
-        className='postBtn'
-        type='submit'
-        disabled={text != '' ? false : true}
-        style={globalStore.submitBtnStyle || submitBtnStyle}
-        onClick={(e) => (text ? handleSubmit(e) : null)}
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          message: '',
+        }}
+        onSubmit={(event, values) => {
+          // Handle form submission here
+          console.log(event);
+          console.log(values);
+        }}
+        validationSchema={toFormikValidationSchema(contactFormSchema)}
       >
-        Post
-      </button>
-    </form>
+        {() => (
+          <form>
+            <div className='userImg' style={imgDiv}>
+              <a
+                target='_blank'
+                href={globalStore.currentUserData.currentUserProfile}
+              >
+                <img
+                  src={
+                    globalStore.customImg ||
+                    customImg ||
+                    globalStore.currentUserData.currentUserImg
+                  }
+                  style={globalStore.imgStyle || imgStyle}
+                  alt='userIcon'
+                  className='imgdefault'
+                />
+              </a>
+            </div>
+            <Form>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your name</span>
+                </label>
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="e.g. John Doe"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <ErrorMessage name="name" component="div" className="text-red-500" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your email</span>
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="e.g johndoe@gmail.com"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <ErrorMessage name="email" component="div" className="text-red-500" />
+              </div>
+              <div className="form-control">
+                <Field
+                  as="textarea"
+                  type="text"
+                  name="message"
+                  className='postComment textarea textarea-bordered h-2'
+                  style={
+                    mode === 'replyMode' || mode === 'editMode'
+                      ? globalStore.replyInputStyle
+                      : globalStore.inputStyle || inputStyle
+                  }
+                  placeholder='Type your reply here.'
+                />
+                <ErrorMessage name="message" component="div" className="text-red-500" />
+              </div>
+              {mode && (
+                <button
+                  className='cancelBtn'
+                  style={globalStore.cancelBtnStyle || cancelBtnStyle}
+                  type='button'
+                  onClick={() =>
+                    mode === 'editMode'
+                      ? globalStore.handleAction(comId, true)
+                      : globalStore.handleAction(comId, false)
+                  }
+                >
+                  Cancel
+                </button>
+              )}
+              <button
+                className='postBtn'
+                type='submit'
+                style={globalStore.submitBtnStyle || submitBtnStyle}
+                onClick={(e)=>handleSubmit(e)}
+              >
+                Post
+              </button>
+            </Form>
+          </form>
+        )}
+      </Formik>
   )
 }
 
